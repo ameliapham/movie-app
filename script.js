0752e03ffa07ed64cb744999f2f1e87616b80627
ameliapham <ptt.huong.8895@gmail.com>
2024-01-14 23:27:42 +0100

    Add APIs key, APIs url and function getMovies

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+fetch(`https://api.themoviedb.org/3/movie/550?api_key=6c0d1fb64932c05b61ac3383d271904f
+`)
+  .then(response => response.json())
+  .then(data => console.log(data));
+*/
+
+const apiKey = "6c0d1fb64932c05b61ac3383d271904f"
+const apiUrl = "https://api.themoviedb.org/3/movie/550?api_key=6c0d1fb64932c05b61ac3383d271904f"
+const movieId = 550
+
+async function getMovies() {
+    const resp = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
+    const respData = await resp.json()
+    console.log(respData)
+    
+    return respData
+}
+
+getMovies()
