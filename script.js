46cabb6884338933ccbb820218ce692fe3f1bfb1
ameliapham <ptt.huong.8895@gmail.com>
2024-01-15 00:24:18 +0100

    Add img path and function to show poster img

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ fetch(`https://api.themoviedb.org/3/movie/550?api_key=6c0d1fb64932c05b61ac3383d2
 
 const apiKey = "6c0d1fb64932c05b61ac3383d271904f"
 const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=6c0d1fb64932c05b61ac3383d271904f"
-const movieId = 550
+// const movieId = 550
+const imgPath = "https://image.tmdb.org/t/p/w500"
+
 
 async function getMovies() {
     //const resp = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
@@ -15,6 +17,13 @@ async function getMovies() {
     const respData = await resp.json()
     console.log(respData)
     
+    respData.results.forEach((movie) => {
+        const img = document.createElement("img")
+        img.src = imgPath + movie.poster_path
+        document.body.appendChild(img)
+    })
+
+
     return respData
 }
 
