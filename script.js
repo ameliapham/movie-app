656900a79791cf6eb52bfbebafa372d97cbe0c3c
ameliapham <ptt.huong.8895@gmail.com>
2024-01-14 23:52:22 +0100

    Update APIs URL

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ fetch(`https://api.themoviedb.org/3/movie/550?api_key=6c0d1fb64932c05b61ac3383d2
 */
 
 const apiKey = "6c0d1fb64932c05b61ac3383d271904f"
-const apiUrl = "https://api.themoviedb.org/3/movie/550?api_key=6c0d1fb64932c05b61ac3383d271904f"
+const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=6c0d1fb64932c05b61ac3383d271904f"
 const movieId = 550
 
 async function getMovies() {
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
+    //const resp = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
+    const resp = await fetch(apiUrl)
     const respData = await resp.json()
     console.log(respData)
     
